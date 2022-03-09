f2b793687a31532d5ff30eba1867ba49c96385d7
John Bryce <johnathanbryce@gmail.com>
2022-03-08 17:37:18 -0800

    worked on the About section -- mostly complete

--- src/components/Layout/About.js
+++ src/components/Layout/About.js
@@ -2,19 +2,61 @@ import React, { useEffect } from "react";
 
 import "aos/dist/aos.css";
 import classes from "./About.module.css";
+import portfolio__image from "../../images/portfolio-image.jpeg";
+import github__Icon from "../../images/github-icon.png";
+import linkedin__icon from "../../images/linkedin-icon.png";
 
 const About = () => {
   return (
     <div className={classes.about}>
       <div className={classes.about__content}>
-        <div>
-          <h2 data-aos="fade-up">Hi! My Name is John Bryce. </h2>
-          <p data-aos="fade-right">
-            Aliqua dolor id mollit dolor sint eiusmod commodo commodo qui enim
-            proident pariatur officia. Non enim commodo commodo voluptate amet
-            veniam nisi sit officia consectetur aliqua aliquip consectetur.
-            Ipsum aliquip esse voluptate tempor consequat et non quis et.
+        <div className={classes.left}>
+          <img
+            data-aos="zoom-in"
+            className={classes.about__bio__img}
+            src={portfolio__image}
+            alt="John Bryce at the beach"
+          />
+        </div>
+
+        <div className={classes.right}>
+          <p data-aos="fade-up">
+            <span className={classes.highlight}>Hey there, </span> my name is
           </p>
+          <h2 data-aos="fade-left"> Johnathan Bryce </h2>
+          <p data-aos="fade-right">
+            and I am a
+            <span className={classes.highlight}> Front-End Web Developer </span>
+            currently based in Vancouver, BC. I am passionate about creating
+            fluid, creative, and interactive websites and applications and
+            expanding on my coding skillsets. Welcome to my portfolio page!
+          </p>
+          <div className={classes.btn__container} data-aos="fade-right">
+            <button className={classes.resume__btn}> Resume </button>
+
+            <a
+              href="https://github.com/johnathanbryce"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={github__Icon}
+                alt="GitHub's icon"
+                className={classes.icon}
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/johnathanbryce/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={linkedin__icon}
+                alt="Linked-In's icon"
+                className={classes.icon}
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
